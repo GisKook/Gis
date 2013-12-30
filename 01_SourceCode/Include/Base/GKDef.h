a950b58ce8108be1170ade304e8d9d03d88646b5
GisKook <zhangkai.gis@163.com>
2013-12-30 16:30:39 +0800

    add head files

---
+++ 01_SourceCode/Include/Base/GKDef.h
@@ -0,0 +1,48 @@
+// 定义些常用的宏，主要是针对C++语言本身的
+#ifndef SOURCECODE_INCLUDE_BASE_GKDEFS_H_H
+#define SOURCECODE_INCLUDE_BASE_GKDEFS_H_H
+
+#include "gksystem.h"
+
+// 一些常量
+#define NULL 0
+#define FALSE 0
+#define TRUE 1
+
+#if ! defined(GK_WIN)
+#if defined(_WIN32) || defined(WIN32)
+#define GK_WIN
+#endif // WIN32 or _WIN32
+#else
+#if defined(__linux__)	
+#define GK_LUX
+#endif // __linux__
+#endif // GK_WIN
+
+// 一些宏
+#ifndef EXTERN_C
+#ifdef __cplusplus
+#define EXTERN_C    extern "C"
+#else
+#define EXTERN_C    extern
+#endif
+#endif
+
+// 字符串相关
+#define _U(s) L ## s
+
+// 用于控制
+#define U_SIZEOF_WCHAR_T 2
+
+#define NAMESPACEBEGIN(x) namespace x{
+#define NAMESPACEEND }
+
+#ifndef IN
+#define IN
+#endif
+
+#ifndef OUT 
+#define OUT 
+#endif
+
+#endif // SOURCECODE_INCLUDE_BASE_GKDEFS_H_H
