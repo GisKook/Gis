2636f14521d5b29b4c41cf7f8e457d689516522c
giskook <zhangkai.gis@163.com>
2014-11-18 11:14:47 +0800

    add fileparse module first to be add is shapefile.

---
+++ 01_SourceCode/Include/FileParseshp/GKFileParseshp.h
@@ -0,0 +1,22 @@
+/*
+ * brief: shp文件转换
+ * function list:
+ * 
+ * author: zhangkai
+ * date: 2014年11月18日
+ */
+#ifndef FILEPARSE_FILEPARSESHP_H_H
+#define FILEPARSE_FILEPARSESHP_H_H
+#include "Base/GKDef.h"
+#include "Base/GKExport.h"
+
+NAMESPACEBEGIN(FILEPARSE)
+
+class FILEPARSE_API GKFileParseshp{
+
+
+};
+
+NAMESPACEEND
+
+#endif
---
+++ 01_SourceCode/Src/FileParseshp/GKFileParseshp.cpp
@@ -0,0 +1,4 @@
+#include "FileParseshp/GKFileParseshp.h"
+#include "shapefile.h"
+
+using namespace FILEPARSE;
